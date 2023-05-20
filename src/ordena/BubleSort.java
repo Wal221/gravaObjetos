9e013734edcae4409f2a4e2a644ffb90ff47fb1c
Wal221 <walmir.cardoso@estudante.ifgoiano.edu.br>
2023-05-20 10:48:43 -0300

    ver numero de trocas agora e possivel

--- src/ordena/BubleSort.java
+++ src/ordena/BubleSort.java
@@ -6,7 +6,9 @@ import java.util.Arrays;
 public class BubleSort {
     public static int[] buble(int array[]) {
         int swap = 0;
-
+        int numberTrocas = 0;
+        int numberCompa = 0 ;
+          numCompara number = new  numCompara();
         //Esse primeiro for , so ira da a volta quando o segundo completa as comparações
         //as comparaçoes e feita da seguinte maneira.
         //a cada volta , seja ela na primeira segundo ou terceira. E comparado a posição no qual o i se encontra
@@ -14,19 +16,21 @@ public class BubleSort {
         //so ira da a volta quando o segundo completar, logo o array[0] que no caso e o valor inicial de  i.
         //vai comparar array[0] com array[j+1]
         for (int i = 0; i < array.length; i++) {
+           numberCompa++;
             //O segundo for caminha normalmente, porem ele so ira realizar a troca se e somente se a condição
             //do if abaixo for verdadeira. Logo se a comparação do if for falsa, o segundo for e incrementado
             // ficando o seguinte ja que o segundo for não foi completado  array[i] > array[j+1]
             //ressaltando novamente que o array[i] esta fixo e so ira caminha quando toda a volta do segundo for
             // ser realizada
-            for (int j = 0; j < array.length; j++)
+            for (int j = 0; j < array.length; j++) {
                 if (array[i] < array[j]) {
                     //A troca e feita da seguinte maneira, se o elemento na posição i for maior que o elemento na
                     // posição j, ele ira flutuar para a ultima posição, para isso necessiatamos da variavel swap
                     //para guarda o valor do array[i] no momento em que a condição if for verdadeira. Para o fazer
                     //flutuar para a proxima posição
-                    swap = array[i];
+                    numberTrocas++;
 
+                    swap = array[i];
                     //como a intenção e ordena de forma crecente ,o array[i] = array[j] ja que o jota e um valor menor
                     //ele deve fica na posição em que se foi comparado e foi visto que ele e um valor menor
                     array[i] = array[j];
@@ -38,15 +42,20 @@ public class BubleSort {
                     //que dada a comparação e necessario trocar se o i for maior
                     array[j] = swap;
                 }
+            }
+
 
 
         }
+        System.out.println("numeros de trocas de "+ numberTrocas);
+        System.out.println("numeros de comparações de "+ numberCompa);
+
 
         return array;
     }
 
     public static void main(String[] args) {
-        int array [] = {4,7,3,1,5};
+        int array [] = {4,7,3,1,5,0};
         buble(array);
 
         System.out.println(Arrays.toString(array));
